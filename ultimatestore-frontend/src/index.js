cb6b44d4fe93eff7de6c18c2ac9f3610ddd0f4c9
jon <jonathan.wong110@gmail.com>
2020-01-22 11:45:44 -0500

    created an actions/products file, reducers/products file, and a store.js file. also took some code out of index and and put it in the store.js file.

--- ultimatestore-frontend/src/index.js
+++ ultimatestore-frontend/src/index.js
@@ -3,23 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-
-const products = () => []
-
-const reducer = combineReducers({
-    products
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_eXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+import store from './store.js'
 
 ReactDOM.render(<Provider store={ store }> <App /> </Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
