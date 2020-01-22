d4f508b99758041f19e313c9e0667aa4fc67a50d
jon <jonathan.wong110@gmail.com>
2020-01-21 23:07:06 -0500

    added the active_model serializer gem, added relationships for the products/reviews models, added validations to the product model, created the serializer folders for products/reviews

--- ultimatestore-backend/app/models/product.rb
+++ ultimatestore-backend/app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    has_many :reviews
+    validates_presence_of :title, :price, :description, :image
 end
---
+++ ultimatestore-backend/app/serializers/product_serializer.rb
@@ -0,0 +1,4 @@
+class ProductSerializer < ActiveModel::Serializer
+  attributes :id, :title, :price, :description, :image
+  has_many :reviews
+end
