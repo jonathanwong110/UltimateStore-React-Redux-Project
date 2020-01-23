e1fa175ab12e456ab8323f46d25c4426a6a40eb8
jon <jonathan.wong110@gmail.com>
2020-01-23 14:52:21 -0500

    changed product.js to display all attributes

--- ultimatestore-frontend/src/components/products/Product.js
+++ ultimatestore-frontend/src/components/products/Product.js
@@ -4,11 +4,13 @@ class Product extends Component {
 
   render() {
     const { product } = this.props;
-
+    
     return (
       <div>
-        <h2 key={product.id}> {product.title}
-        </h2>
+        <img alt={product.id} src={product.image}> </img>
+        <h2 key={product.id}> {product.title} </h2>
+        <h3 key={product.id}> {product.price} </h3>
+        <p key={product.id}> {product.description} </p>
       </div>
     );
   }
