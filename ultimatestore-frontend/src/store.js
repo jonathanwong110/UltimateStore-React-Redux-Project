67e27a2b7533a2aa482ab61293e2fa1e51848a8e
jon <jonathan.wong110@gmail.com>
2020-01-30 14:59:43 -0500

    created a combineReducer for the products and cart reducer, put some functions into cartActions

--- ultimatestore-frontend/src/store.js
+++ ultimatestore-frontend/src/store.js
@@ -1,9 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import manageProducts from './reducers/manageProducts.js'
+import manageCart from './reducers/manageCart.js'
 import thunk from 'redux-thunk'
+
+const reducer = combineReducers({
+    products: manageProducts,
+    cart: manageCart
+})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(manageProducts, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 export default store
