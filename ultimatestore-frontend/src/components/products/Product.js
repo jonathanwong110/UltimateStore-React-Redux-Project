be77c84fe90616c6e64e6dff3a77808812a0c699
jon <jonathan.wong110@gmail.com>
2020-02-04 14:52:43 -0500

    changed the Product component from a class component to a functional component

--- ultimatestore-frontend/src/components/products/Product.js
+++ ultimatestore-frontend/src/components/products/Product.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card } from 'react-bootstrap';
 import ShowProduct from './ShowProduct'
 
-class Product extends Component {
-
-  render() {
+export default function Product(props) {
     
-    const { product } = this.props;
-    
-    return (
-      <Card style={{ width: '15rem' }}>
-        <Card.Img variant="top" src={product.image} height="200px" width="auto"/>
-        <Card.Body>
-          <Card.Title>{product.title}</Card.Title>
-          <Card.Text>
-            ${product.price}
-          </Card.Text>
-          <ShowProduct products={this.props}/>
-        </Card.Body>
-      </Card>
-    );
-  }
-};
-
-export default Product;
+  const { product } = props;
+  
+  return (
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src={product.image} height="200px" width="auto"/>
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+          ${product.price}
+        </Card.Text>
+        <ShowProduct products={props}/>
+      </Card.Body>
+    </Card>
+  );
+}
