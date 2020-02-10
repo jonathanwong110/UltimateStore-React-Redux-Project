023cf8dc25c07e2069db382edb2bb62431dc3de2
jon <jonathan.wong110@gmail.com>
2020-02-10 17:03:16 -0500

    took localstorage out of the cart reducer

--- ultimatestore-frontend/src/reducers/manageCart.js
+++ ultimatestore-frontend/src/reducers/manageCart.js
@@ -2,37 +2,16 @@ export default function manageCart (state = {
     cart: []
 }, action) {
     switch (action.type) {
-        case 'LOAD_CART':
-            localStorage.getItem('cartproducts', JSON.stringify({
-                ...state,
-                cart: [...state.cart]
-            }))
-        return {
-                ...state,
-                cart: [...state.cart]
-        }
         case 'ADD_TO_CART':
             let addedProduct = Object.values(action.payload)
-            let newCart = JSON.parse(localStorage.cartproducts).cart
-            console.log(newCart)
-                localStorage.setItem('cartproducts', JSON.stringify({
-                    ...state,
-                    cart: [...state.cart, addedProduct]
-                }))
-            return {
-                    ...state,
-                    cart: [...state.cart, addedProduct]
-            }
-        case 'REMOVE_FROM_CART':
-            let currentCart = JSON.parse(localStorage.cartproducts).cart
-            currentCart = currentCart.filter(cartproduct => cartproduct[0] !== action.payload)
-                localStorage.setItem('cartproducts', JSON.stringify({
-                    cart: currentCart
-                }))
             return {
                 ...state,
-                cart: [...state.cart]
-        }
+                cart: [...state.cart, addedProduct]
+            }
+        case 'REMOVE_FROM_CART':
+            return {
+                ...state, cart: state.cart.filter(product => product[0] !== action.payload)
+            }
         default:
             return state
     }
