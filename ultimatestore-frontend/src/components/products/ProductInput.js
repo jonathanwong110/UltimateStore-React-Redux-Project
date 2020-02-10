3b61e8711f11e2ffe061f9b0dd7da28d626dbdde
jon <jonathan.wong110@gmail.com>
2020-02-10 00:39:13 -0500

    used react-bootstrap to make the input form look more appealing

--- ultimatestore-frontend/src/components/products/ProductInput.js
+++ ultimatestore-frontend/src/components/products/ProductInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, Button, Col } from 'react-bootstrap'
 
 class ProductInput extends Component {
 
@@ -17,6 +18,7 @@ class ProductInput extends Component {
     this.setState({
         [e.target.name]: e.target.value
     });
+    console.log('e.target.value', e.target.value)
   }
 
   handleCategoryChange = (e) => {
@@ -40,38 +42,49 @@ class ProductInput extends Component {
 
   render() {
     return (
-      <div>
-        <br></br>
-        Create a Product!
-        <form onSubmit={e => this.handleSubmit(e)}>
-        <br></br>
-          <label>
-              Title: 
-              <input type="text" name="title" onChange={e => this.handleChange(e)} value={this.state.title}></input> 
-          </label> 
-          <label>
-              Price: 
-              <input type="text" name="price" onChange={e => this.handleChange(e)} value={this.state.price}></input>
-          </label>
-          <label>
-              Description: <input type="text" name="description" onChange={e => this.handleChange(e)} value={this.state.description}></input> 
-          </label>
-          <label>
-              Image: <input type="text" name="image" onChange={e => this.handleChange(e)} value={this.state.image}></input> 
-          </label>
-          <label>
-          Category:
-          <select type="select" onChange={e => this.handleCategoryChange(e)} value={this.state.category}>
-            <option value="Apparel">Apparel</option>
-            <option value="Electronics">Electronics</option>
-            <option value="Miscellaneous">Miscellaneous</option>
-          </select>
-        </label>
-          <input type="submit" value="Create Product"></input>
-        </form>
-        <br></br>
-        <br></br>
-      </div>
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <Form>
+          Create a Product!
+          <br></br>
+          <br></br>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridTitle">
+              <Form.Label>Title: </Form.Label>
+                <input type="text" name="title" placeholder="Title" onChange={e => this.handleChange(e)} value={this.state.title}></input> 
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPrice">
+              <Form.Label>Price: </Form.Label>
+                <input type="text" name="price" placeholder="Price" onChange={e => this.handleChange(e)} value={this.state.price}></input>
+            </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridDescription">
+              <Form.Label>Description: </Form.Label>
+                <input type="text" name="description" placeholder="Description" onChange={e => this.handleChange(e)} value={this.state.description}></input>
+            </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridImage">
+            <Form.Label>Image URL: </Form.Label>
+              <input type="text" name="image" placeholder="Image URL" onChange={e => this.handleChange(e)} value={this.state.image}></input> 
+          </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridCategory">
+              <Form.Label>Category: </Form.Label>
+              <select type="select" onChange={e => this.handleCategoryChange(e)} value={this.state.category}>
+                <option value="Apparel">Apparel</option>
+                <option value="Electronics">Electronics</option>
+                <option value="Miscellaneous">Miscellaneous</option>
+              </select>
+            </Form.Group>
+          </Form.Row>
+
+          <Button variant="success" type="submit">
+            Create Product
+          </Button>
+          <br></br>
+          <br></br>
+        </Form>
+      </form>
     );
   }
 };
