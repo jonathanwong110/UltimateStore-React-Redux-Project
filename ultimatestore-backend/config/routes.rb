2aa6f3cd64ebfb37b67e14d4a622a28be63f7899
jon <jonathan.wong110@gmail.com>
2020-02-10 16:54:56 -0500

    took out cart/cartproduct controllers/model/serializers/db, routes in the backend and took out unnecessary methods in the products/reviews controller

--- ultimatestore-backend/config/routes.rb
+++ ultimatestore-backend/config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   resources :carts
   namespace :api do
     namespace :v1 do
-
-      get '/cart' => 'carts#show', as: 'api/v1/carts'
-
+      
       resources :products
       resources :reviews
-      resources :carts, only: [:show, :destroy]
-      resources :cartproducts, only: [:show]
     end
   end
 end
